6e38fc9b5f9b3ecf859308fd784f77c2f18af994
ShunsukeHKD <shun.hkd@gmail.com>
2020-11-12 04:05:21 +0900

    [追加]チャンネル一括ダウンロード機能を追加

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ using System.Text.RegularExpressions;
 using System.Reflection;
 using System.Linq;
 using System.Data;
+using Nicome.WWW.API.Types.Comment.CommentBody.Json;
+using System.Threading.Tasks;
 
 namespace Nicome
 {
@@ -83,20 +85,45 @@ namespace Nicome
                 return 100;
             }
             var storeData = store.GetData();
-
-            //実行
-            var downloader = new WWW.Comment.Comment();
-            errorlevel=downloader.DownloadComment(storeData.GetNicoID()).Result;
-
-            switch (errorlevel)
+            List<string> IdList;
+            try
             {
-                case NicoEnums::GenelicErrorCode.ERROR:
-                    logger.Log("処理を終了します。");
-                    return 101;
-                default:
-                    logger.Log("処理を終了します。");
-                    return 0;
+                IdList = CLI.IDHandler.GetIDLists().Result;
             }
+            catch(Exception e)
+            {
+                logger.Error($"ダウンロードIDの解析中にエラーが発生しました。(詳細: {e.Message})");
+                return 102;
+            }
+
+            if (IdList.Count > 1)
+            {
+                logger.Log($"id:{IdList[0]}ほか、{IdList.Count}件の動画をダウンロードします");
+            }
+
+            foreach (var id in IdList)
+            {
+
+                //実行
+                var downloader = new WWW.Comment.Comment();
+                errorlevel = downloader.DownloadComment(id).Result;
+
+                switch (errorlevel)
+                {
+                    case NicoEnums::GenelicErrorCode.ERROR:
+                        logger.Log("処理を終了します。");
+                        return 101;
+                }
+
+                if (IdList.Count > 1)
+                {
+                    logger.Log("待機中です...(5,000ms)");
+                    Task.Delay(5000).Wait();
+                }
+            }
+
+            logger.Log("処理を終了します)");
+            return 0;
         }
 
         private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
